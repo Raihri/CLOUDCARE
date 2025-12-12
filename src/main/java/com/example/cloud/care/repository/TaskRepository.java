d1f864e4860e84b79a491dfd930eb8a2d107d3b5
Samarina Sarwar Sachi <samarinasachi12@gmail.com>
2025-12-13 00:50:52 +0600

    Matha3

---
+++ src/main/java/com/example/cloud/care/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.cloud.care.repository;
+
+import com.example.cloud.care.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
