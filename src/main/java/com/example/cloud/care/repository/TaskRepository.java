4df2cae18de1634216e53582eb6dedaed7512cfa
Samarina Sarwar Sachi <samarinasachi12@gmail.com>
2025-12-22 20:00:58 +0600

    Remove file from repoo1

--- src/main/java/com/example/cloud/care/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cloud.care.repository;
-
-import com.example.cloud.care.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
